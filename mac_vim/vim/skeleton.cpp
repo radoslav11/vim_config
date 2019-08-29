78fcbe24e0ba077f448af7ef9df917ab6578f06d
Radoslav Dimitrov <r.dimitrov@ocado.com>
2019-08-29 11:42:45 +0300

    Add macOS vim configuration.

---
+++ mac_vim/vim/skeleton.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+#define SZ(x) ((int)x.size())
+#define ALL(V) V.begin(), V.end()
+#define L_B lower_bound
+#define U_B upper_bound
+#define pb push_back
+
+using namespace std;
+template<class T, class T1> int chkmin(T &x, const T1 &y) { return x > y ? x = y, 1 : 0; }
+template<class T, class T1> int chkmax(T &x, const T1 &y) { return x < y ? x = y, 1 : 0; }
+const int MAXN = (1 << 20);
+
+void read() 
+{
+
+}
+
+void solve() 
+{
+
+}
+
+int main() 
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	read();
+	solve();
+	return 0;
+}
